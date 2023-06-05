1edeb5a699aa8cd9ce409adf288d36f510c9163b
Suyash <suyashmishra370@gmail.com>
2023-06-05 20:07:03 +0530

    cart error resolved , used async-await

--- models/cart.js
+++ models/cart.js
@@ -8,37 +8,38 @@ const p = path.join(
 );
 
 module.exports = class Cart {
-  static addProduct(id, productPrice) {
-    // Fetch the previous cart
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        console.log(err);
-      } else {
-        let cart = { products: [], totalPrice: 0 };
-        if (fileContent) {
+  static async addProduct(id, productPrice) {
+    try {
+      const fileContent = await fs.promises.readFile(p);
+      let cart = { products: [], totalPrice: 0 };
+      if (fileContent) {
+        try {
           cart = JSON.parse(fileContent);
+        } catch (jsonErr) {
+          console.log("Error parsing JSON:", jsonErr);
         }
-        // Analyze the cart => Find existing product
-        const existingProductIndex = cart.products.findIndex(
-          (prod) => prod.id === id
-        );
-        const existingProduct = cart.products[existingProductIndex];
-        let updatedProduct;
-        // Add new product/ increase quantity
-        if (existingProduct) {
-          updatedProduct = { ...existingProduct };
-          updatedProduct.qty = updatedProduct.qty + 1;
-          cart.products = [...cart.products];
-          cart.products[existingProductIndex] = updatedProduct;
-        } else {
-          updatedProduct = { id: id, qty: 1 };
-          cart.products = [...cart.products, updatedProduct];
-        }
-        cart.totalPrice = cart.totalPrice + +productPrice;
-        fs.writeFile(p, JSON.stringify(cart), (err) => {
-          console.log(err);
-        });
       }
-    });
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+
+      cart.totalPrice = cart.totalPrice + +productPrice;
+
+      await fs.promises.writeFile(p, JSON.stringify(cart));
+    } catch (err) {
+      console.log("Error:", err);
+    }
   }
 };
