3b94aadf8539d20c8aab2d659639a2da26730be2
Suyash <suyashmishra370@gmail.com>
2023-06-05 19:22:40 +0530

    cart error resolved

--- models/cart.js
+++ models/cart.js
@@ -11,30 +11,34 @@ module.exports = class Cart {
   static addProduct(id, productPrice) {
     // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
-      // Analyze the cart => Find existing product
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
-      );
-      const existingProduct = cart.products[existingProductIndex];
-      let updatedProduct;
-      // Add new product/ increase quantity
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty = updatedProduct.qty + 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
-      } else {
-        updatedProduct = { id: id, qty: 1 };
-        cart.products = [...cart.products, updatedProduct];
-      }
-      cart.totalPrice = cart.totalPrice + +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      if (err) {
         console.log(err);
-      });
+      } else {
+        let cart = { products: [], totalPrice: 0 };
+        if (fileContent) {
+          cart = JSON.parse(fileContent);
+        }
+        // Analyze the cart => Find existing product
+        const existingProductIndex = cart.products.findIndex(
+          (prod) => prod.id === id
+        );
+        const existingProduct = cart.products[existingProductIndex];
+        let updatedProduct;
+        // Add new product/ increase quantity
+        if (existingProduct) {
+          updatedProduct = { ...existingProduct };
+          updatedProduct.qty = updatedProduct.qty + 1;
+          cart.products = [...cart.products];
+          cart.products[existingProductIndex] = updatedProduct;
+        } else {
+          updatedProduct = { id: id, qty: 1 };
+          cart.products = [...cart.products, updatedProduct];
+        }
+        cart.totalPrice = cart.totalPrice + +productPrice;
+        fs.writeFile(p, JSON.stringify(cart), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 };
