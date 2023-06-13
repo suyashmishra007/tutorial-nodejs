91ef8ec3849138a0c9b9531ffb2790b0d74316dd
Suyash Mishra <suyash.mishra@vlinkinfo.com>
2023-06-13 13:30:30 +0530

    mongo error fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
-
+const PORT= 8000;
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
@@ -71,7 +71,7 @@ mongoose
       }
     });
     console.log("CONNECTED");
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((err) => {
     console.log(err);
--- controllers/admin.js
+++ controllers/admin.js
@@ -82,7 +82,7 @@ exports.getProducts = (req, res, next) => {
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
-      console.log(products);
+      // console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
